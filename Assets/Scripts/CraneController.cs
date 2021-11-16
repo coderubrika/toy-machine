18d1e0f38fe02d1fc559ec56bd677682ff6af8f7
Anton Samartsev <samarcev.anton1@gmail.com>
2021-11-16 06:59:55 +0300

    crane started

---
+++ Assets/Scripts/CraneController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using UltEvents;
+using UnityEngine;
+
+namespace Assets.Scripts
+{
+    public class CraneController : MonoBehaviour
+    {
+        private UltEvent OnGrabStart;
+        private UltEvent OnGrabEnd;
+
+        [SerializeField]
+        private SpringJoint mount;
+
+        [SerializeField] 
+        private HingeJoint[] fingers;
+
+        public void Grab()
+        {
+
+        }
+
+        private void FixedUpdate()
+        {
+            // опустить руку
+            // сжать клешки
+            // поднять руку
+
+            // тут проще разделить это на несколько классов и использовать их по отдельности
+        }
+    }
+}
